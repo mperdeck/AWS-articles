4f60b9c8f7c351b5344c53bda9d48a6e8ce141ec
mperdeck <mperdeck@gmail.com>
2015-03-18 19:39:06 +1100

    initial

---
+++ SimpleSiteWithDb/SimpleSiteWithDb/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleSiteWithDb.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public string ISDN { get; set; }
+
+    }
+}
