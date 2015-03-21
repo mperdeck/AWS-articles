7f4b0f3eda68ad67513839449f67fbdf5c32215b
mperdeck <mperdeck@gmail.com>
2015-03-21 18:02:21 +1100

    3

---
+++ SimpleSite/SimpleSite/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Threading;
+
+namespace SimpleSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Heavy()
+        {
+            Thread.Sleep(5000);
+            return View();
+        }
+    }
+}
