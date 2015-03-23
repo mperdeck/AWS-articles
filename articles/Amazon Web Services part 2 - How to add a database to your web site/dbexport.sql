bfcd5afc0ed945cf3b8f4ad504fff71ac9210b3e
mperdeck <mperdeck@gmail.com>
2015-03-23 19:46:44 +1100

    5

---
+++ articles/Amazon Web Services part 2 - How to add a database to your web site/dbexport.sql
@@ -0,0 +1,4 @@
+Process started at 3/22/2015 9:29:34 PM -- UTC -> 3/22/2015 10:29:34 AM ... 
+No analysis done on script.
+Processing finished at 3/22/2015 9:29:37 PM -- UTC -> 3/22/2015 10:29:37 AM
+Total processing time: 0 hours, 0 minutes and 3 seconds
