443102facce64eac85c68a741c08669bb6ba1e6d
mperdeck <mperdeck@gmail.com>
2015-04-25 17:20:30 +1000

    cf12

--- SimpleSiteWithDb/SimpleSiteWithDb/Deploy.ps1
+++ SimpleSiteWithDb/SimpleSiteWithDb/Deploy.ps1
@@ -2,22 +2,22 @@
 # and must have property "Copy to Output Directory" set to "Copy always".
 # That way it will be packaged and deployed.
 
+Function WaitUntilWebsiteReachesState([string]$siteName, [string]$state)
+{
+	do { 
+		Start-Sleep -m 50; 
+		$webSiteState = (cmd /c %systemroot%\system32\inetsrv\appcmd list site $siteName /text:state) | Out-String
+	} while ($webSiteState.Trim() -ne $state)
+}
+
+
+
+
     Write-Host ">>> dewploy"
 
+# Find the directory that this script is running in
 $scriptpath = $MyInvocation.MyCommand.Path
-$scriptpath
 $scriptDir = Split-Path $scriptpath
-$scriptDir
-
-    Write-Host "<<< dewploy"
-return
-
-
-
-
-
-cd $scriptDir
-
 
 # -------------------
 add-windowsfeature web-webserver -includeallsubfeature -logpath $env:temp\webserver_addrole.log
@@ -29,20 +29,24 @@ cmd /c %systemroot%\system32\inetsrv\appcmd stop site /site.name:"Default Web Si
 
 # Give it an application pool that runs .Net 4.0
 cmd /c %systemroot%\system32\inetsrv\appcmd add apppool /name:"v4.0" /managedRuntimeVersion:"v4.0" /managedPipelineMode:Integrated
-Set-ItemProperty 'IIS:\Sites\Default Web Site' ApplicationPool "v4.0"
+cmd /c %systemroot%\system32\inetsrv\appcmd set site /site.name:"Default Web Site" "/[path='/'].applicationPool:v4.0"
 
-############## wait unitl web site stopped. Then wipe its files
-do { Start-Sleep -m 50; $defaultWebSiteState = Get-ItemProperty 'IIS:\Sites\Default Web Site' State } while ($defaultWebSiteState.Value -ne "Stopped")
+# Wait until the web site has stopped
+do { 
+	Start-Sleep -m 50; 
+	$defaultWebSiteState = (cmd /c %systemroot%\system32\inetsrv\appcmd list site "Default Web Site" /text:state) | Out-String
+	$defaultWebSiteState
+} while ($defaultWebSiteState -ne "Stopped")
 
-# remove all files from the Default Web Site
+# Remove all its files
 $physicalPath = Get-ItemProperty 'IIS:\Sites\Default Web Site' physicalPath
-$physicalPath = [System.Environment]::ExpandEnvironmentVariables("$physicalPath") + '\*'
-Remove-Item $physicalPath -recurse
+$physicalPathContent = [System.Environment]::ExpandEnvironmentVariables("$physicalPath") + '\*'
+Remove-Item $physicalPathContent -recurse
 
 # Deploy the files to the root directory of the Default Web Site
-CMD /C release.deploy.cmd /Y  "-setParam:kind=TextFile,scope=\\web.config$,match=@@DbServer@@,value='xxx yyy zz-----z' -setParam:kind=TextFile,scope=\\web.config$,match=@@DbUsername@@,value='xxx yyy z----zz2aaa' -setParam:kind=TextFile,scope=\\web.config$,match=@@DbPassword@@,value='xxx yyy zzz----3aaaaa' -setparam:name='IIS Web Application Name',kind=providerpath,scope=apphostconfig,value='default web site'"
+copy-item "$scriptDir\*" $physicalPath -force -recurse
+# CMD /C release.deploy.cmd /Y  "-setParam:kind=TextFile,scope=\\web.config$,match=@@DbServer@@,value='xxx yyy zz-----z' -setParam:kind=TextFile,scope=\\web.config$,match=@@DbUsername@@,value='xxx yyy z----zz2aaa' -setParam:kind=TextFile,scope=\\web.config$,match=@@DbPassword@@,value='xxx yyy zzz----3aaaaa' -setparam:name='IIS Web Application Name',kind=providerpath,scope=apphostconfig,value='default web site'"
 
-# Start the Default Web Site
-start-website -name "Default Web Site"
+# Start the web site again
 cmd /c %systemroot%\system32\inetsrv\appcmd start site /site.name:"Default Web Site"
 
