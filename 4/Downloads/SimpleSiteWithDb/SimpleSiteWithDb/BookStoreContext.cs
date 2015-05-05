bb94f962642594143e030d1361a87cf2b01a06fc
mperdeck <mperdeck@gmail.com>
2015-05-05 21:46:53 +1000

    reducing path lengths

---
+++ 4/Downloads/SimpleSiteWithDb/SimpleSiteWithDb/BookStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using SimpleSiteWithDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SimpleSiteWithDb
+{
+    public class BookStoreContext: DbContext
+    {
+        public BookStoreContext(string connString)
+            : base(connString)
+        {
+        }
+        
+        public DbSet<Book> Books { get; set; } 
+    }
+}
